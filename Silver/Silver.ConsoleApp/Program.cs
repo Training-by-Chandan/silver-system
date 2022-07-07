089a7caf2fe6f54a611f51f2727c1153de9feb83
Chandan Gupta Bhagat <chandan.bhagat@outlook.com>
2022-07-07 07:46:52 +0545

    console project added with gitignore

---
+++ Silver/Silver.ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Silver.ConsoleApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Nepal!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
