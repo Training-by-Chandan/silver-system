ff889bed3e25b126b405e5169a315f6b3333957a
Chandan Gupta Bhagat <chandan.bhagat@outlook.com>
2022-07-08 07:49:26 +0545

    datatpes explained

--- Silver/Silver.ConsoleApp/Program.cs
+++ Silver/Silver.ConsoleApp/Program.cs
@@ -2,14 +2,59 @@
 
 namespace Silver.ConsoleApp
 {
-    public class Program
+    internal class Program
     {
-        public static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Nepal!");
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
+            DataTypes();
         }
+
+        private static void Casting()
+        {
+            //implicit
+            //explicit
+            //parsing
+            //type conversion
+        }
+
+        private static void DataTypes()
+        {
+            //int
+            int i = 2;
+            //uint
+            uint ui = 4000000000;
+            //long
+            long l = 1000000000000000000;
+            //ulong
+            //short
+            short s = -32768;
+            //ushort
+            ushort us = 65535;
+            //float / Single
+            float f = 2.092f;
+            //double
+            double d = 2.98d;
+            //decimal
+            decimal de = 6.78m;
+            //byte
+            byte b = 255;
+            //bool
+            bool bo = true;
+            //char
+            char c = 'C';
+            //string
+            string str = "ABCD";
+            //datetime
+            DateTime dt = DateTime.Now;
+
+            int num = 'C';
+        }
+
+        //SOLID
+        //S : Single Responsibility Principle
+        //O : Open - Closed Principle
+        //L : Liskov Subsitution Principle
+        //I : Interface Segregation Principle
+        //D : Dependency Inversion Principle
     }
 }
