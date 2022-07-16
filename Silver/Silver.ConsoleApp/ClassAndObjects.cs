b01e288eb59cc0ecc8b22b538ad556e191fcb255
s-t-a-y <gorkhalitarang@outlook.com>
2022-07-16 17:06:17 +0545

    Class Object

---
+++ Silver/Silver.ConsoleApp/ClassAndObjects.cs
@@ -0,0 +1,46 @@
+﻿namespace Silver.ConsoleApp
+{
+    // class design or blueprint
+    // object instance of class
+    public class ClassAndObjects
+    {
+        // 1. Special Function
+        // 2. Name same as Class name
+        // 3. No return type
+        // 4. Runs only once in its life cycle
+        // 5. We can define multiple constructor but use only one to create single object
+        public ClassAndObjects()
+        {
+        }
+
+        // Constructor Overloading
+        public ClassAndObjects(int i)
+        {
+        }
+
+        public ClassAndObjects(int i, string x)
+        {
+        }
+
+        public ClassAndObjects(string x, int i)
+        {
+        }
+    }
+}
+
+// Constructor
+// Variables
+// Properties
+// Functions or Methods Actions in MVC
+
+// Delegates
+// Events
+// Destructor
+
+// Inheritance
+// Polymorphism
+// Interface
+// Abstract Class
+// Static Class
+// Tuples
+// Templates
--- Silver/Silver.ConsoleApp/Program.cs
+++ Silver/Silver.ConsoleApp/Program.cs
@@ -6,8 +6,11 @@ namespace Silver.ConsoleApp
     {
         private static void Main(string[] args)
         {
-            DType();
+            //DType();
+            BranchingEx1();
         }
+
+        #region Basic
 
         private static void DType()
         {
@@ -18,11 +21,59 @@ namespace Silver.ConsoleApp
             short sa = -32768;
             ushort ss = 65535;
 
+            //Convert.ToInt32
+
+            //string str = "123";
+            // int i = int.Parse(str);
             //SOLID
+            // f = (float) d;
             // implicit and explicit
             // type conversion
             // parsing
             // casting
+        }
+
+        #endregion Basic
+
+        private static void BranchingEx1()
+        {
+            Console.WriteLine("Enter Number");
+            var input = Console.ReadLine();
+            var num = Convert.ToInt32(input);
+            var res = "";
+
+            //linq query ternary
+
+            if (num % 2 == 0) Console.WriteLine("Even");
+            else Console.WriteLine("Odd");
+
+            var result = (num % 2 == 0) ? "Even" : "Odd";
+        }
+
+        private static void BranchEx2()
+        {
+            for (; ; )
+            {
+            }
+            for (int i = 0; i < 10;)
+            {
+            }
+            for (int i = 0; true; i++)
+            {
+            }
+        }
+
+        //loop known and unknown
+        // do while while / known or unknown ( most prefer )
+        // for loop and for each / known quantities
+
+        private static void ClassAndObjects()
+        {
+            ClassAndObjects cs = new ClassAndObjects();
+            ClassAndObjects css = new ClassAndObjects(5);
+            ClassAndObjects cs1 = new ClassAndObjects(1, "");
+            ClassAndObjects cs2 = new ClassAndObjects("", 1);
+            cs = new ClassAndObjects();
         }
     }
 }
