2d974dae62426e99468c3c658470473f6f7a165d
s-t-a-y <gorkhalitarang@outlook.com>
2022-07-20 19:26:32 +0545

    branch update

--- Silver/Silver.ConsoleApp/ClassAndObjects.cs
+++ Silver/Silver.ConsoleApp/ClassAndObjects.cs
@@ -1,51 +1,3 @@
-<<<<<<< HEAD
-﻿namespace Silver.ConsoleApp
-{
-    // class design or blueprint
-    // object instance of class
-    public class ClassAndObjects
-    {
-        // 1. Special Function
-        // 2. Name same as Class name
-        // 3. No return type
-        // 4. Runs only once in its life cycle
-        // 5. We can define multiple constructor but use only one to create single object
-        public ClassAndObjects()
-        {
-        }
-
-        // Constructor Overloading
-        public ClassAndObjects(int i)
-        {
-        }
-
-        public ClassAndObjects(int i, string x)
-        {
-        }
-
-        public ClassAndObjects(string x, int i)
-        {
-        }
-    }
-}
-
-// Constructor
-// Variables
-// Properties
-// Functions or Methods Actions in MVC
-
-// Delegates
-// Events
-// Destructor
-
-// Inheritance
-// Polymorphism
-// Interface
-// Abstract Class
-// Static Class
-// Tuples
-// Templates
-=======
 ﻿using System;
 
 namespace Silver.ConsoleApp
@@ -219,4 +171,3 @@ namespace Silver.ConsoleApp
         //parallel programming
     }
 }
->>>>>>> ddd817494e8a258375dcb30939b03c3fb7afa31c
--- Silver/Silver.ConsoleApp/Program.cs
+++ Silver/Silver.ConsoleApp/Program.cs
@@ -7,76 +7,6 @@ namespace Silver.ConsoleApp
     {
         private static void Main(string[] args)
         {
-<<<<<<< HEAD
-            //DType();
-            BranchingEx1();
-        }
-
-        #region Basic
-
-        private static void DType()
-        {
-            int a = 10;
-            long l = 5000;
-            ulong ul = 5555;
-            short s = 32767;
-            short sa = -32768;
-            ushort ss = 65535;
-
-            //Convert.ToInt32
-
-            //string str = "123";
-            // int i = int.Parse(str);
-            //SOLID
-            // f = (float) d;
-            // implicit and explicit
-            // type conversion
-            // parsing
-            // casting
-        }
-
-        #endregion Basic
-
-        private static void BranchingEx1()
-        {
-            Console.WriteLine("Enter Number");
-            var input = Console.ReadLine();
-            var num = Convert.ToInt32(input);
-            var res = "";
-
-            //linq query ternary
-
-            if (num % 2 == 0) Console.WriteLine("Even");
-            else Console.WriteLine("Odd");
-
-            var result = (num % 2 == 0) ? "Even" : "Odd";
-        }
-
-        private static void BranchEx2()
-        {
-            for (; ; )
-            {
-            }
-            for (int i = 0; i < 10;)
-            {
-            }
-            for (int i = 0; true; i++)
-            {
-            }
-        }
-
-        //loop known and unknown
-        // do while while / known or unknown ( most prefer )
-        // for loop and for each / known quantities
-
-        private static void ClassAndObjects()
-        {
-            ClassAndObjects cs = new ClassAndObjects();
-            ClassAndObjects css = new ClassAndObjects(5);
-            ClassAndObjects cs1 = new ClassAndObjects(1, "");
-            ClassAndObjects cs2 = new ClassAndObjects("", 1);
-            cs = new ClassAndObjects();
-=======
             var res = "n";
             do
             {
@@ -94,12 +24,11 @@ namespace Silver.ConsoleApp
                 //OperatorOverloadingExample();
                 //StringManipulation();
                 //StaticAndNOnStatic();
-                //InheritanceExample();
+                InheritanceExample();
 
                 Console.WriteLine("Do you want to continue more? (y/n)");
                 res = Console.ReadLine();
             } while (res == "y");
->>>>>>> ddd817494e8a258375dcb30939b03c3fb7afa31c
         }
 
         #region Inheritance
@@ -121,7 +50,7 @@ namespace Silver.ConsoleApp
             }
         }
 
-        private static IShape _getShape(int choice)
+        private static IShape GetShape(int choice)
         {
             //IShape s = new Circle();
             //IShape squareShape = new Square();
@@ -605,7 +534,7 @@ namespace Silver.ConsoleApp
 
             string str = i.ToString();
             //Program p = new();
-            str = p.ToString();
+            //str = p.ToString();
 
             //parsing
             str = "123";
