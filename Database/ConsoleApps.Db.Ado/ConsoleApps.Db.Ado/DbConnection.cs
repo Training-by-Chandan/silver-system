607c03d8e00566ddb3b4a9e4b64007ec2489031d
Chandan Gupta Bhagat <chandan.bhagat@outlook.com>
2022-08-11 07:43:02 +0545

    console project added with ADO for database

---
+++ Database/ConsoleApps.Db.Ado/ConsoleApps.Db.Ado/DbConnection.cs
@@ -0,0 +1,107 @@
+﻿using System.Data.SqlClient;
+
+namespace ConsoleApps.Db.Ado
+{
+    public class DbConnection
+    {
+        public static readonly string _connectionString = "server=DESKTOP-PT71T7O\\SQLBHAGAT;database=Silver.Console.Db.Ado;integrated security=true";
+        public static void ReadAll()
+        {
+            ///1. Create the query
+            string query= "select * from person";
+
+            ReadData(query);
+        }
+
+        public static void GetById()
+        {
+            Console.WriteLine("Enter the ID");
+            var id = Convert.ToInt32(Console.ReadLine());
+            ///1. Create the query
+            string query = "select * from person where id = " + id;
+
+            ReadData(query);
+        }
+
+        private static void ReadData(string query)
+        {
+            ///2. create sql connection object
+            SqlConnection con = new SqlConnection(_connectionString);
+
+            ///3. Create sql command object
+            SqlCommand cmd = new SqlCommand(query, con);
+
+            ///4.open the connection
+            con.Open();
+
+            ///5. Execute commands 
+            var res = cmd.ExecuteReader();
+
+            Console.WriteLine("Data from the database");
+            Console.WriteLine("==============================================");
+            ///6. do the work
+            while (res.Read())
+            {
+                Console.WriteLine($"Id : {res.GetFieldValue<int>(0)}");
+                Console.WriteLine($"Name : {res.GetFieldValue<string>(1)}");
+                Console.WriteLine($"Email : {res.GetFieldValue<string>(2)}");
+                Console.WriteLine("==============================================");
+            }
+
+            ///7. close the connection
+            con.Close();
+        }
+
+        public static void Create()
+        {
+            Console.WriteLine("Enter the Name");
+            var name = Console.ReadLine();
+            Console.WriteLine("Enter the Email");
+            var email = Console.ReadLine();
+
+            string query = $"Insert into person (Name, Email) values ('{name}','{email}')";
+
+            RunNonQuery(query);
+        }
+
+        public static void Edit()
+        {
+            Console.WriteLine("Enter the Id");
+            var id = Console.ReadLine();
+            Console.WriteLine("Enter the Name");
+            var name = Console.ReadLine();
+            Console.WriteLine("Enter the Email");
+            var email = Console.ReadLine();
+
+            string query = $"Update person set Name='{name}', Email = '{email}' where id={id}";
+
+            RunNonQuery(query);
+        }
+        public static void Delete()
+        {
+            Console.WriteLine("Enter the Id");
+            var id = Console.ReadLine();
+            string query = $"delete from person where id={id}";
+
+            RunNonQuery(query);
+        }
+
+        private static void RunNonQuery(string query)
+        {
+            ///2. create sql connection object
+            SqlConnection con = new SqlConnection(_connectionString);
+
+            ///3. Create sql command object
+            SqlCommand cmd = new SqlCommand(query, con);
+
+            ///4.open the connection
+            con.Open();
+
+            ///5. Execute commands 
+            var res = cmd.ExecuteNonQuery();
+
+            ///6. close the connection
+            con.Close();
+        }
+    }
+}
---
+++ Database/ConsoleApps.Db.Ado/ConsoleApps.Db.Ado/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace ConsoleApps.Db.Ado
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var res = "Y";
+            do
+            {
+                Console.WriteLine("Press\n1. to Read all records\n2 to Get by ID\n3 to Create the record\n4 to Edit the records\n5 to Delete the records");
+                var choice = Convert.ToInt32(Console.ReadLine());
+                SelectChoice(choice);
+                
+                Console.WriteLine("Do you want to continue more? (Y/N)");
+                res = Console.ReadLine();
+            } while (res.ToUpper()=="Y");
+        }
+
+        private static void SelectChoice(int choice)
+        {
+            switch (choice)
+            {
+                case 1:
+                    DbConnection.ReadAll();
+                    break;
+                case 2:
+                    DbConnection.GetById();
+                    break;
+                case 3:
+                    DbConnection.Create();
+                    break;
+                case 4:
+                    DbConnection.Edit();
+                    break;
+                case 5:
+                    DbConnection.Delete();
+                    break;
+                default:
+                    Console.WriteLine("Wrong choice");
+                    break;
+            }
+        }
+    }
+}
