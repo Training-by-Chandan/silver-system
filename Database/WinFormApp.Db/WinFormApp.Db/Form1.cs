7779be3089638ce337bc7677e0c377539103ba5b
Chandan Gupta Bhagat <chandan.bhagat@outlook.com>
2022-08-15 07:50:45 +0545

    Desktop application started

---
+++ Database/WinFormApp.Db/WinFormApp.Db/Form1.cs
@@ -0,0 +1,16 @@
+namespace WinFormApp.Db
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var text = textBox1.Text;
+           label1.Text = text;
+        }
+    }
+}
