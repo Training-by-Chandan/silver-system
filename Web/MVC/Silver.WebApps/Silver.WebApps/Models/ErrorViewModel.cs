e7ab5f764429ae64c0b093a755d92af7d7a5341f
Chandan Gupta Bhagat <chandan.bhagat@outlook.com>
2022-08-31 07:47:42 +0545

    asp.net core web app added

---
+++ Web/MVC/Silver.WebApps/Silver.WebApps/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Silver.WebApps.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
