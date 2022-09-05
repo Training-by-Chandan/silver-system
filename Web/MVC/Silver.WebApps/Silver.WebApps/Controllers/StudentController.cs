01622600b067b930c09347a30586dc7492be21b4
Chandan Gupta Bhagat <chandan.bhagat@outlook.com>
2022-09-05 07:57:30 +0545

    CRUD operation without scaffolding

---
+++ Web/MVC/Silver.WebApps/Silver.WebApps/Controllers/StudentController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Silver.WebApps.Data;
+using Silver.WebApps.Models;
+
+namespace Silver.WebApps.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly ApplicationDbContext db;
+
+        public StudentController(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var data = db.Students.ToList();
+            return View(data);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var data = db.Students.Find(id);
+            return View(data);
+        }
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            var data = db.Students.Find(id);
+            return View(data);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var data = db.Students.Find(id);
+            return View(data);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Student model)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Students.Add(model);
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Student model)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Students.Update(model);
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Student model)
+        {
+            var existing = db.Students.Find(model.Id);
+                db.Students.Remove(existing);
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+           
+        }
+    }
+}
